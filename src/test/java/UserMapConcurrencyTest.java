62e1cb2bedc0283bdcc8ee477e416f68af4071c8
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-26 20:10:09 +0100

    tests: mutable map

--- src/test/java/UserMapConcurrencyTest.java
+++ src/test/java/UserMapConcurrencyTest.java
@@ -1,8 +1,10 @@
 import org.junit.Test;
 
-import java.util.Map;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,29 +13,35 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by mtumilowicz on 2019-01-25.
  */
 public class UserMapConcurrencyTest {
-
-    private Map<Integer, User> userMap = Map.of(
-            1, new User(1, 40),
-            2, new User(2, 33)
-    );
-
     @Test
     public void write_transfer() {
+        var userMap = Stream.of(
+                new User(1, 40),
+                new User(2, 33)
+        ).collect(Collectors.toMap(User::getId, Function.identity()));
+
         ReadWriteLock lock = new ReentrantReadWriteLock();
         LockExecutor executor = new LockExecutor(lock);
 
-        var transferred = executor.write(() -> {
+        executor.write(() -> {
             var transfer = PositiveInt.of(15);
-            return Map.of(1, userMap.get(1).outcome(transfer),
-                    2, userMap.get(2).income(transfer));
+            userMap.replace(1, userMap.get(1).outcome(transfer));
+            userMap.replace(2, userMap.get(2).income(transfer));
+
+            return Void.class;
         });
 
-        assertThat(transferred.get(1).getBalance(), is(25));
-        assertThat(transferred.get(2).getBalance(), is(48));
+        assertThat(userMap.get(1).getBalance(), is(25));
+        assertThat(userMap.get(2).getBalance(), is(48));
     }
 
     @Test
     public void read_sum_balance() {
+        var userMap = Stream.of(
+                new User(1, 40),
+                new User(2, 33)
+        ).collect(Collectors.toMap(User::getId, Function.identity()));
+
         ReadWriteLock lock = new ReentrantReadWriteLock();
         LockExecutor executor = new LockExecutor(lock);
 
